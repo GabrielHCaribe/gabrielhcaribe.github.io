40cc24ee2df8d01cdb8099df63a2aadbec257122
GabrielHCaribe <gabe.h.c@hotmail.com>
2026-01-13 01:21:40 -0500

    Initial project descriptions added

--- data/projects.ts
+++ data/projects.ts
@@ -17,9 +17,9 @@ export const projects: Project[] = [
     title: "CAN-RXG Team Cristar",
     shortDescription: "Electrical lead researching laser-induced crystallization in 0-g.",
     content: [
-      "This project involved a deep dive into numerical analysis, specifically exploring wave function collapse using Python and NumPy.",
-      "As the Electrical Lead, my primary focus was component selection and ensuring the stability of the laser system under varying gravitational loads.",
-      "The results provided significant data for our research into microgravity physics."
+      "As part of the CAN-RXG Team Cristar, we are designing and building an apparatus to study laser-induced crystallization in microgravity aboard a Falcon 20. We are currently in the preliminary design phase of the project, with the flight scheduled for June, 2026.",
+      "As the Electrical Lead, I am responsible for electrical component selection, securing sponsorships, designing and building the GUI, and testing to ensure flight-readiness.",
+      "The results will help determine whether short duration parabolic flights are a viable option for improved crystal growth, supporting industries that rely on high-quality crystals such as the pharmaceutical industry."
     ],
     tags: ["Electrical Design", "Component Selection", "Research"],
     image: "/canrgx.png"
@@ -27,9 +27,11 @@ export const projects: Project[] = [
   {
     id: "1",
     title: "Advanced Physics Lab",
-    shortDescription: "Particle identification, optical traps, and super conductor analysis.",
+    shortDescription: "High-Temp super conductor analysis and particle identification",
     content: [
-      "A short, concise description for this specific project."
+      "Last semester as part of my Advanced Physics Lab course, I conducted performed experiments and analyzed data relating to determining the critical temperature of an unknown high-temperature super conductor and particle identification in bubble chamber images.",
+      "1. In the high-temperature super conductor experiment, I cooled an a BSCCO sample using liquid nitrogen and plotted its resitance against temperature using a 4 wire method to overcome the wire's internal resistance. I also measued the magnetic susceptability of the sample as it cooled, using aa two coil method. I analyzed the data using a Savitzky-Golay filter and Monte-Carlo simulations, and calculated a critical temperature that aligns with literature values.",
+      "2. For the bubble chamber experiment, I analyzed two strong interactions in images taken at the Stanford Linear Accelerator center, and deterined all particles involved using conservation properties. This experiment was much more data-heavy, having tracked numerous calculated and inferred particle properteis for dozens of bubble chamber tracks in Excel. In the end, I successfully identified all particles involved in both interactions, with momentum and energy being conserved."
     ],
     tags: ["Research", "Documentation", "Physics"],
     image: "/levetation.png"
@@ -39,7 +41,11 @@ export const projects: Project[] = [
     title: "PID Heating System",
     shortDescription: "Engineering tool for calculating internal forces in beam structures.",
     content: [
-      "A short, concise description for this specific project."
+      "As part of my internship at the Max Planck Institute for the Structure and Dynamics of Matter, I designed, constructed, and tested a custom PID heating system.",
+      "The goal was to maintain the temperatrue of the small sample of glycerol used in a laser ablation experiment to analyze rubber-like elastic properties of Newtonian fluids.",
+      "The system was composed of an industrial level Arduino (Industruino), temperature transducers, a relay,PT-1000 thermometers, and a resistive heater. I designed the electrical system and perfomed claulcations as to required power and componenet ratings.",
+      "I wrote all the code for the PID controller from scratch in Arduino-C, and tested the entire system to ensure it met the required temperature stability of ±0.3K, meeting system requirements.",
+      "To document the project, I wrote an 18-page user manual detailing the saftey consideration, electrical design, software, and user instructions."
     ],
     tags: ["Electrical Design", "Arduino-C", "Testing"],
     image: "/box.jpg"
@@ -65,8 +71,10 @@ export const projects: Project[] = [
     id: "4",
     title: "Software Projects",
     shortDescription: "Engineering tool for calculating internal forces in beam structures.",
-    content: [
-      "A short, concise description for this specific project."
+    content: ["Description coming soon!"
+      // "1.",
+      // "2. Sema carving project: \nDeveloped a C program to reduce image size while maintaining visual integrity using the seam carving algorithm. Implemented dynamic programming techniques to identify and remove low-energy seams, optimizing for horizontal and vertical reductions. The program processes images in PPM format, allowing users to specify target dimensions. Successfully reduced image sizes by up to 50% while preserving key visual features, demonstrating proficiency in C programming and algorithmic problem-solving.",
+
     ],
     tags: ["MATLAB", "Python", "C"],
     image: "/pde.jpg"
@@ -76,7 +84,9 @@ export const projects: Project[] = [
     title: "Bridge Design",
     shortDescription: "Engineering tool for calculating internal forces in beam structures.",
     content: [
-      "A short, concise description for this specific project."
+      "Wrote MATLAB code to iterate through different bridge designs quickly and perdict failure mode and load.",
+      "Final design withstood over 100lbs, outperforming 80% of other designs in the class.",
+      "Detailed documentation of design process and final analysis compiled into a comprehensive report."
     ],
     tags: ["Statics", "MATLAB", "Documentation"],
     image: "/bridge.jpg"
